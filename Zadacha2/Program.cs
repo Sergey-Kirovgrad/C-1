4170930ced0beeeb8ec40fd419cdcc5b57cceda8
Sergey <arakcheev1@yandex.ru>
2022-12-21 23:08:48 +0500

    new file

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿//программа, которая на вход принимает два числа и выдаёт, 
+//какое число больше.
+Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine($"{a}");
+}else
+{
+    Console.WriteLine($"{b}");
+}
---
+++ Zadacha4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+Console.Write("Введите 3 числа:");
+Console.Write("a:");
+int num_a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int num_b = int.Parse(Console.ReadLine());
+Console.Write("c:");
+int num_c = int.Parse(Console.ReadLine());
+int max = num_a;
+if(num_b>max){
+    max = num_b;
+}
+if(num_c>max){
+    max = num_c;
+}
+    Console.WriteLine($"Наибольшее число {max}");
